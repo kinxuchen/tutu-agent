8af96ceb0a77e4d941da0dbcae56707e726b5fc9
xuchen01 <kingxc1991@foxmail.com>
2025-03-30 10:11:17 +0800

    git: 修改变量名

--- agents/order/agent.py
+++ agents/order/agent.py
@@ -1,16 +1,15 @@
 from langchain.agents import AgentType
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import StateGraph, START, END
-from typing import Dict, Optional, Annotated, List, Any
+from typing import Dict, Optional, Annotated, List, Any, override
 from langchain_core.messages import BaseMessage
 from langchain_core.tools import tool
 from langchain.callbacks.base import BaseCallbackHandler
 import operator
-from langchain_core.callbacks import BaseCallbackManager
 from constant import SQL_URL, db
 from langchain_community.agent_toolkits import create_sql_agent
 from sql_toolkit import SQLDatabaseToolkit
-from agents.order.prompts import client_templates
+from agents.order.prompts import clientele_templates, SQL_PREFIX
 from llm import llm
 import jsonpickle
 from uuid import UUID
@@ -29,6 +28,19 @@ sql_tools = SQLDatabaseToolkit(
 )
 
 
+class ToolCallbackHandle(BaseCallbackHandler):
+    @override
+    def on_tool_end(
+            self,
+            output: Any,
+            *,
+            run_id: UUID,
+            parent_run_id: Optional[UUID] = None,
+            **kwargs: Any,
+    ):
+        print('output', output)
+
+tool_callback = ToolCallbackHandle()
 # openai-tools
 sql_agent = create_sql_agent(
     llm=llm,
@@ -36,24 +48,8 @@ sql_agent = create_sql_agent(
     agent_executor_kwargs={
         "return_intermediate_steps": True,
     },
-    prefix="""
-        You are an agent designed to interact with a SQL database.
-        Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-        You need to analyze every sentence from the user, as their first statement might involve querying customer information.  
-        You should strive to search across multiple related tables as comprehensively as possible.
-        You must return the primary key of each table.
-        If an index exists in the table, you should use it for queries to optimize performance.
-        Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-        You can order the results by a relevant column to return the most interesting examples in the database.
-        Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-        You have access to tools for interacting with the database.
-        Only use the below tools. Only use the information returned by the below tools to construct your final answer.
-        You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-        
-        DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-        
-        If the question does not seem related to the database, just return "I don't know" as the answer.
-        """,
+    callbacks=[tool_callback],
+    prefix=SQL_PREFIX,
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 )
 
@@ -62,16 +58,16 @@ sql_agent = create_sql_agent(
 class OrderState(Dict):
     messages: Annotated[List[BaseMessage], operator.add]
     result: Optional[str] = None
-    client: Optional[str] = None # 需要查询的客户信息
+    clientele: Optional[str] = None # 需要查询的客户信息
     query_client: Optional[Annotated[List[Any], operator.add]] = None # 查询到的客户信息
     query_order: Optional[Annotated[List[Any], operator.add]] = None # 查询到的库存信息
 
 order_graph = StateGraph(OrderState)
 
 # 尝试从用户输入中提取出客户的信息
-def initial_client_node(state: OrderState):
+def initial_clientele_node(state: OrderState):
     prompts = ChatPromptTemplate.from_messages([
-        ('system', client_templates),
+        ('system', clientele_templates),
         ('user', """
             begin!
             {input}
@@ -86,11 +82,11 @@ def initial_client_node(state: OrderState):
     print('result', result)
     if result and result.content != '':
         return {
-            'client': result.content
+            'clientele': result.content
         }
     return state
 
-def query_client_node(state: OrderState):
+def query_clientele_node(state: OrderState):
     last_message = state['messages'][-1]
     new_state = state.copy()
     new_state['query_client'] = []
@@ -108,7 +104,7 @@ def query_client_node(state: OrderState):
         for tool in result['intermediate_steps']:
             if tool and isinstance(tool, tuple) and tool[0] is not None and hasattr(tool[0], 'tool'):
                 if tool[0].tool == 'sql_db_query' and tool[1] != '':
-                    new_state['query_client'].append(jsonpickle.decode(tool[1]))
+                    new_state['query_client'] = new_state['query_client'] + jsonpickle.decode(tool[1])
         return new_state
     except Exception as e:
         return new_state
@@ -125,17 +121,20 @@ def query_inventory_node(state: OrderState):
         for tool in result['intermediate_steps']:
             if tool and isinstance(tool, tuple) and tool[0] is not None and hasattr(tool[0], 'tool'):
                 if tool[0].tool == 'sql_db_query' and isinstance(tool[1], str) and tool[1] != '':
-                    new_state['query_order'].append(jsonpickle.decode(tool[1]))
+                    new_state['query_order'] =  new_state['query_order']  + jsonpickle.decode(tool[1])
         return new_state
     except Exception as e:
         return new_state
-order_graph.add_node('query_inventory_node', query_inventory_node)
-order_graph.add_node('query_client_node', query_client_node)
-order_graph.add_node('initial_client_node', initial_client_node)
 
-order_graph.add_edge(START, 'initial_client_node')
-order_graph.add_edge('initial_client_node', 'query_client_node')
-order_graph.add_edge('query_client_node', 'query_inventory_node')
+
+
+order_graph.add_node('query_inventory_node', query_inventory_node)
+order_graph.add_node('query_clientele_node', query_clientele_node)
+order_graph.add_node('initial_clientele_node', initial_clientele_node)
+
+order_graph.add_edge(START, 'initial_clientele_node')
+order_graph.add_edge('initial_clientele_node', 'query_clientele_node')
+order_graph.add_edge('query_clientele_node', 'query_inventory_node')
 
 order_graph.add_edge('query_inventory_node', END)
 
--- agents/order/prompts.py
+++ agents/order/prompts.py
@@ -1,9 +1,28 @@
 from langchain_core.prompts import ChatPromptTemplate
 
 
-client_templates = """
+clientele_templates = """
     你是我的语义分析助手，你需要尝试从用户的输入中，提取可能是人名或者机构名称的信息。
     需要特别注意：
     1. 需要特别注意用户输入的头部信息，用户可能在句首添加用户或者机构信息
     2. 你只要输出识别到到机构或者用户信息，禁止输出其他多余内容
+"""
+
+SQL_PREFIX = """
+    You are an agent designed to interact with a SQL database.
+        Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+        You need to analyze every sentence from the user, as their first statement might involve querying customer information.  
+        You should strive to search across multiple related tables as comprehensively as possible.
+        You must return the primary key of each table.
+        If an index exists in the table, you should use it for queries to optimize performance.
+        Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
+        You can order the results by a relevant column to return the most interesting examples in the database.
+        Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+        You have access to tools for interacting with the database.
+        Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+        You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+        
+        DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+        
+        If the question does not seem related to the database, just return "I don't know" as the answer.
 """
