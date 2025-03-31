3709d83f94c4e983e39f9cf97d69c9b8f0371e36
xuchen01 <kingxc1991@foxmail.com>
2025-03-31 12:21:27 +0800

    init：调试参数

--- llm.py
+++ llm.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 from constant import BASE_URL, API_KEY, MODEL_NAME
 
 llm = ChatOpenAI(
@@ -6,4 +7,9 @@ llm = ChatOpenAI(
     model=MODEL_NAME,
     api_key=API_KEY,
     base_url=BASE_URL
+)
+
+
+ds_llm = ChatDeepSeek(
+    model='deepseek-reasoner'
 )
